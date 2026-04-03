bb0879f24fc196034137cbe329cd14aa85bf6dd5
Stef4Ny <Stef4Ny@192.168.53.242>
2026-04-03 20:20:40 +0300

    primul prototip (defapt doar interfata si cateva maruntisuri)

---
+++ src/interfata_drive/Main.java
@@ -0,0 +1,10 @@
+package interfata_drive;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        // Folosim Application.launch pentru a porni corect ciclul de viață JavaFX
+        Application.launch(FFShareApp.class, args);
+    }
+}
